6860008febe5a9091338aed17aa7805f2196d004
tarunaditya91 <shankertarunaditya369@gmail.com>
2024-05-13 18:30:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='wasteDetector',
+    version='0.0.0',
+    author='shanker tarun aditya',
+    author_email='tarunaditya91@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
